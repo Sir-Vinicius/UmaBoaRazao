6be46399b4afabc8aabc12229948598e097c1061
Brendo094 <brendo.camara094@gmail.com>
2026-09-03 19:00:22 -0300

    Melhoria na camera

--- Assets/Scripts/Camera/RailCamera.cs
+++ Assets/Scripts/Camera/RailCamera.cs
@@ -6,13 +6,14 @@ public class RailCamera : MonoBehaviour
     private int nodeCount;
     [SerializeField] private GameObject camera;
     [SerializeField] private int nodesNumero;
-    [SerializeField] private float vel;
+    [SerializeField] private float vel = 2f;
+    [SerializeField] private float velOlhar = 2f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         nodeCount = transform.childCount;
         nodes = new Vector3[nodeCount];
-
+        camera = GameObject.Find("Main Camera");
 
         for (int i = 0; i < nodeCount; i++)
         {
@@ -23,20 +24,31 @@ public class RailCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // tem que criar o objeto da rail e seu filhos que são os waypoints
-        Vector3 railAlvo = nodes[nodesNumero];
-        Vector3 posicaoAtual = camera.transform.position;
+        MoveCamera();
 
-        Vector3 direcao = (railAlvo - posicaoAtual).normalized;
-
-        camera.transform.position = posicaoAtual + direcao * vel * Time.deltaTime;
-     
         if (nodeCount > 1)
         {
             for (int i = 0; i < nodeCount - 1; i++)
             {
                 Debug.DrawLine(nodes[i], nodes[i + 1], Color.red);
             }
+        }
+    }
+
+    private void MoveCamera()
+    {
+        // tem que criar o Empty "Rail" e criar os filhos "Waypoint", e colocar o script no Empty GameObject.
+        Vector3 railAlvo = nodes[nodesNumero];
+        Vector3 posicaoAtual = camera.transform.position;
+        var visaoAtual = camera.transform.rotation;
+
+        Vector3 direcao = (railAlvo - posicaoAtual).normalized;
+        Quaternion direcaoOlhar = Quaternion.LookRotation(direcao);
+
+        if (Vector3.Distance(posicaoAtual, railAlvo) > 0.1f)
+        {
+            camera.transform.position = posicaoAtual + direcao * vel * Time.deltaTime;
+            camera.transform.rotation = Quaternion.Slerp(camera.transform.rotation, direcaoOlhar, velOlhar * Time.deltaTime);
         }
     }
 }
