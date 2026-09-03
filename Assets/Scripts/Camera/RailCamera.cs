3213e7f66a3632d0c55ca02be83e87f50c7a682f
Brendo094 <brendo.camara094@gmail.com>
2026-09-03 16:41:28 -0300

    Adição do script da camera on rail

---
+++ Assets/Scripts/Camera/RailCamera.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class RailCamera : MonoBehaviour
+{
+    private Vector3[] nodes;
+    private int nodeCount;
+    [SerializeField] private GameObject camera;
+    [SerializeField] private int nodesNumero;
+    [SerializeField] private float vel;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        nodeCount = transform.childCount;
+        nodes = new Vector3[nodeCount];
+
+
+        for (int i = 0; i < nodeCount; i++)
+        {
+            nodes[i] = transform.GetChild(i).position;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 railAlvo = nodes[nodesNumero];
+        Vector3 posicaoAtual = camera.transform.position;
+
+        Vector3 direcao = (railAlvo - posicaoAtual).normalized;
+
+        camera.transform.position = posicaoAtual + direcao * vel * Time.deltaTime;
+     
+        if (nodeCount > 1)
+        {
+            for (int i = 0; i < nodeCount - 1; i++)
+            {
+                Debug.DrawLine(nodes[i], nodes[i + 1], Color.red);
+            }
+        }
+    }
+}
+
