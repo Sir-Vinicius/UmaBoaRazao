c45a8c9964ab4f7d25405695d85ffbeb3bb5a6a4
Sir-Vinicius <viniciusidai@gmail.com>
2026-09-03 21:41:22 -0300

    Adicionando dano no tiro

--- Assets/Scripts/Player/PlayerAim.cs
+++ Assets/Scripts/Player/PlayerAim.cs
@@ -4,6 +4,9 @@ using UnityEngine.InputSystem;
 public class PlayerAim : MonoBehaviour
 {
     [SerializeField] private RectTransform crosshair;
+    [SerializeField] private Camera playerCamera;
+    [SerializeField] private float shootDistance = 100f;
+    [SerializeField] private int dano = 1;
     void Start()
     {
         
@@ -14,5 +17,30 @@ public class PlayerAim : MonoBehaviour
         Vector2 mousePosition = Mouse.current.position.ReadValue();
 
         crosshair.position = mousePosition;
+
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            Atirar();
+        }
+    }
+
+    private void Atirar()
+    {
+        Vector2 mousePosition = Mouse.current.position.ReadValue();
+        Ray ray = playerCamera.ScreenPointToRay(mousePosition);
+
+        if (Physics.Raycast(ray, out RaycastHit hit, shootDistance))
+        {
+            Debug.Log("Acertou: " + hit.collider.name +
+                  " | VidaController: " +
+                  hit.collider.GetComponentInParent<VidaController>());
+
+            VidaController vida = hit.collider.GetComponentInParent<VidaController>();
+
+            if (vida != null)
+            {
+                vida.ReceberDano(dano);
+            }
+        }
     }
 }
