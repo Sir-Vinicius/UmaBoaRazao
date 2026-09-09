c5691b0b451b78f75f4e3f3e82f98516646f4c53
Vinicius dos Santos <97066925+Sir-Vinicius@users.noreply.github.com>
2026-09-09 14:12:51 -0300

    billboard e prefebs de environment (#18)
    Adicionando billboarding e novos prefabs

--- Assets/Scripts/Enemy/SpawnerController.cs
+++ Assets/Scripts/Enemy/SpawnerController.cs
@@ -80,7 +80,6 @@ public class SpawnerController : MonoBehaviour
     void SpawnEnemy(int nodeIndex, float alvo, bool voltando)
     {
         GameObject enemy = Instantiate(meuEnemy, wayPoints[nodeIndex], Quaternion.identity);
-        Enemy enemyScript = enemy.GetComponent<Enemy>();
         inimigos.Add(enemy);
         enemyScript.pontoX = alvo;
         enemyScript.voltandoEnemy = voltando;
