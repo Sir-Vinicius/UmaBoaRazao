6c0fa5c0ad246d03273ea90cf35bcd9ddb3cc901
Brendo094 <brendo.camara094@gmail.com>
2026-09-09 12:09:22 -0300

    Feat/movimento enemy (#17)
    * Novos prefebs
    
    * Add no movimento do inimigo

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -5,19 +5,41 @@ public class Enemy : MonoBehaviour
 {
     private SpawnerController listaEnemy;
 
+    [SerializeField] private Vector3 posicaoOriginal;
+    public float pontoX;
+        
+    public Vector3 pontoAlvo;
+    [SerializeField] private float vel = 2f;
+    private float intervalo;
+
     public BoxCollider meuBox;
+
+    public bool indoParaAlvo = true;
+    public bool voltandoEnemy;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         meuBox = GetComponent<BoxCollider>();
         listaEnemy = FindAnyObjectByType<SpawnerController>();
+
+        posicaoOriginal = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
+        pontoAlvo = new Vector3(pontoX, posicaoOriginal.y, posicaoOriginal.z);
 
-    }
+        if (indoParaAlvo)
+        {
+            PrepararAtirar();
+        }
+
+        if (!indoParaAlvo && voltandoEnemy)
+        {
+            VoltarPosicaoOriginal();
+        }
+     }
 
     private void OnDestroy()
     {
@@ -26,5 +48,43 @@ public class Enemy : MonoBehaviour
             listaEnemy.inimigos.Remove(gameObject);
         }
     }
+    public void PrepararAtirar()
+    {
 
+
+        Vector3 direcao = (pontoAlvo - transform.position).normalized;
+
+        if (Vector3.Distance(transform.position, pontoAlvo) > 0.1f)
+        {
+            transform.position = transform.position + direcao * vel * Time.deltaTime;
+        }
+        
+    }
+    public void VoltarPosicaoOriginal()
+    {
+        
+        Vector3 direcao = (posicaoOriginal - transform.position).normalized;
+
+        if (Vector3.Distance(transform.position, posicaoOriginal) > 0.1f)
+        {
+            transform.position = transform.position + direcao * vel * Time.deltaTime;
+            
+        }
+
+
+        if (Vector3.Distance(transform.position, posicaoOriginal) < 0.1f)
+        {
+            if (intervalo == 0)
+            {
+                intervalo = Random.Range(1f, 3f);
+                intervalo = Time.time + intervalo;
+            }
+
+            if (Time.time > intervalo)
+            {
+                indoParaAlvo = true;
+                intervalo = 0;
+            }
+        }
+     }
 }
--- Assets/Scripts/Enemy/EnemyAttack.cs
+++ Assets/Scripts/Enemy/EnemyAttack.cs
@@ -3,27 +3,45 @@ using UnityEngine;
 public class EnemyAttack : MonoBehaviour
 {
     [SerializeField] private int dano = 2;
-    [SerializeField] private float intervaloAtaque = 2f;
+    [SerializeField] private float intervaloAtaque;
 
     private float proximoAtaque;
     private VidaController vidaPlayer;
     private CoverController coverController;
+    private Enemy enemy;
+    public bool arrivedAtTarget = true;
 
     void Start()
     {
         GameObject player = GameObject.Find("Player");
         vidaPlayer = player.GetComponent<VidaController>();
         coverController = player.GetComponent<CoverController>();
+
+        enemy = GetComponent<Enemy>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Time.time >= proximoAtaque)
+
+
+        if (Vector3.Distance(transform.position, enemy.pontoAlvo) < 0.1f)
         {
-            Atacar();
-            proximoAtaque = Time.time + intervaloAtaque;
+            if (arrivedAtTarget)
+            {
+                intervaloAtaque = Random.Range(1f, 3f);
+                proximoAtaque = Time.time + intervaloAtaque;
+                arrivedAtTarget = false;
+            }
+
+            if (Time.time >= proximoAtaque)
+            {
+                Atacar();
+                arrivedAtTarget = true;
+                enemy.indoParaAlvo = false;
+            }
         }
+       
     }
 
     private void Atacar()
